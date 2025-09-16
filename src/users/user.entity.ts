d927e72fe098c73a2190f3852aca6866dc5d9524
abdelazizhany66 <adhmh080@gmail.com>
2025-09-16 07:22:20 +0200

    feat(user): add full CRUD, entities, DTOs and database setup
    • Added db.sqlite file for local development
    • Created dto folder with CreateUserDto & UpdateUserDto
    • Created entity folder with User entity
    • Added reports folder for future reporting features
    • Implemented CRUD operations (create, findOne, findAll by email, update, remove)
    • Used NotFoundException for better error handling

---
+++ src/users/user.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, Column,PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from "typeorm";
+import { Exclude } from 'class-transformer'
+
+@Entity()
+export class User{
+  @PrimaryGeneratedColumn()
+  id:number
+
+  @Column()
+  email:string
+  
+  @Column()
+  @Exclude()
+  password:string
+
+  @AfterInsert()
+  logInsert(){
+    console.log('Inserted User With Id', this.id)
+  }
+
+  @AfterUpdate()
+  logUpdate(){
+    console.log('Updated User With Id', this.id)
+  }
+
+  @AfterRemove()
+  logRemove(){
+    console.log('Removed User With Id', this.id)
+  }
+
+}
